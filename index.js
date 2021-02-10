6790ff20eb04accbd9f8776e3c6d8cd076134c8c
Open Studios <rajveer2007ng@gmail.com>
2021-02-10 17:15:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const client = new Discord.Client();
 const syncToken = 'YOUR BOT TOKEN HERE!';
 client.login(syncToken);
 client.on('ready', ()=>{
-  console.log('Online! Sync Codes!')
+  console.log('Online! Open Studios!')
 })
 
 client.on("message", async message => {
 const chatcord = require('chatcord');
 const chat = new chatcord.Client()
 //You can change "sync-codes" to any name you want for the channel
-if (message.channel.name != 'sync-codes') return;
+if (message.channel.name != 'open-studios') return;
 chat.chat(message.cleanContent).then(reply => {
     if (message.author.bot) return;
     //Send's a reply
